73eaef20fc467fb254a995023d6b34c9c48fb022
Busta <mattybusta06@gmail.com>
2023-09-03 18:28:47 +0100

    updated basics project

--- src/basics.js
+++ src/basics.js
@@ -23,4 +23,133 @@ console.log(a)
 a = 7
 b = a;
 
-console.log(a)
+console.log(a) // use console log to check parts of your program
+
+// initialize these three variables ( This is like algebra)
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c " string!";
+
+// declorations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments     (Must have correct spelling)
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//Adding number 
+
+var sum 10 + 10;
+console.log(sum)
+
+// subtracting number
+
+var difference = 45 - 33; //could also be in brackets
+
+// multiplying numbers
+
+var product = 8 * 10; // product will be the result number
+
+// dividing numbers
+
+var quotient = 66 / 33;
+
+// incrementing numbers
+
+var myVar = 87;
+
+//only change code below this line 
+myVar++;
+myVar += 2 //can use this to change the number it goes up by 
+
+//decrementing numbers
+var myVar = 11;
+
+//only change code below this
+myVar = myVar - 1;
+myVar--;
+
+//decimal number
+
+var ourdecimal = 5.7
+
+//only change below this line 
+
+var mydecimal = 0.009 //use this to use decimals wherever
+
+//multiply decimals
+
+var product = 2.0 * 2.5
+console.log(product) //use this to run in terminal
+
+//divide decimals
+
+var quotient = 4.4 / 2.2
+
+//finding a remainder 
+
+var remainder;
+remainder = 11 % 3;
+
+//Compund assignment with augmented addition
+
+// shortcut for a = a + 12;
+a += 12;
+
+//subtraction 
+a -= 12;
+
+//multiply
+a *= 12;
+
+//division
+a /= 12;
+
+//declare string variables
+//example
+var firstname = "alan"
+var lastname = "turing"
+
+var myFirstName = "matty"
+var myFirstName = "busta"
+
+//escaping literal quotes in strings
+var myStr = "I am a "double quoted"string inside"double quotes";
+var myStr = "I am a \"double quoted\"string inside\"double quotes"; // backslash (\) keeps extending the quote rather than 3 sepreate its one big quote 
+console.log(myStr)
+
+//escape sequences in strings
+
+/*****
+CODE OUTPUT
+\'      single quote 
+\''     double quote 
+\\      backslash 
+\n      newline 
+\r      carriage return
+\t      tab
+\b      backspace
+\f      form feed
+*****/
+
+var myStr = "FirstLine/n\t\\SecondLine\nThirdLine"
+
+//concatenating strings with plus operator
+
+//example
+var ourStr = "I come first. " + "I come second.";
+
+//only change code below this line 
+
+var myStr: "This is the start. " + "This is the end."  // this is to add multiple sentences together (comes out as this is the start. This is the end.)
+console. log(myStr);
+
